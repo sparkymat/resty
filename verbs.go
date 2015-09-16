f3bad297a6b9e97cec5578a430b3f7af0fafed42
Ajith Hussain <csy0013@googlemail.com>
2015-09-16 15:40:02 +1000

    Removed .json. This is not Rails yo!

--- verbs.go
+++ verbs.go
@@ -16,6 +16,7 @@ type Verb struct {
 	name       string
 	methods    []shttp.Method
 	actionType ActionType
+	format     string
 }
 
 var Create = Verb{name: "create", methods: []shttp.Method{shttp.Put}, actionType: CollectionAction}
@@ -43,14 +44,14 @@ func CollectionVerb(methods []shttp.Method, verb string) Verb {
 func (verb Verb) routeSuffix() string {
 	switch verb.name {
 	case "create", "index":
-		return ".json"
+		return ""
 	case "show", "update", "destroy":
-		return "/{id:[0-9]+}.json"
+		return "/{id:[0-9]+}"
 	}
 
 	if verb.actionType == MemberAction {
-		return fmt.Sprintf("/{id:[0-9]+}/%v.json", verb.name)
+		return fmt.Sprintf("/{id:[0-9]+}/%v", verb.name)
 	} else {
-		return fmt.Sprintf("/%v.json", verb.name)
+		return fmt.Sprintf("/%v", verb.name)
 	}
 }
